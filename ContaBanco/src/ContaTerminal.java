02ca4218ed0e995ee86f5cda57b5781853490939
VH-S <116882856+VH-S@users.noreply.github.com>
2023-09-26 07:50:49 -0300

    novo projeto

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,6 +1,27 @@
+import java.util.Scanner;
+
 
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite seu primeiro nome");
+        String nome = scanner.next();
+
+        System.out.println("Digite seu sobrenome");
+        String sobrenome = scanner.next();
+
+        System.out.println("Digite o número da sua conta");
+        int numero = scanner.nextInt();
+
+        System.out.println("Digite o número de sua agência");
+        String agencia = scanner.next();
+
+        System.out.println("Digite seu saldo");
+        double saldo = scanner.nextDouble();
+
+        scanner.close();
+
+        System.out.println("Olá " + nome+ " " + sobrenome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque");
     }
 }
